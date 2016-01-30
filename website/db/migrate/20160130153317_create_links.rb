be4996de1da5bd1794482e42fed1b11609531892
Anthoney Tsou <anttsou@gmail.com>
2016-01-30 17:09:02 +0000

    removed ruby website

--- website/db/migrate/20160130153317_create_links.rb
+++
@@ -1,9 +0,0 @@
-class CreateLinks < ActiveRecord::Migration
-  def change
-    create_table :links do |t|
-      t.string "url", :null => false
-      t.text "raw", :limit => 4294967295
-      t.timestamps
-    end
-  end
-end
